7fe792c8797e914488f97701fc5c7376bc336378
Jesse Pollak <jpollak92@gmail.com>
2013-06-19 14:02:49 -0700

    production db to postgres

--- app.py
+++ app.py
@@ -9,8 +9,13 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import requests
 import os
 import json
+import functools
+import time
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    'HEROKU_POSTGRESQL_WHITE_URL',
+    'sqlite:////tmp/test.db'
+)
 DEBUG = True
 CLEF_APP_ID = '4f318ac177a9391c2e0d221203725ffd'
 CLEF_APP_SECRET = '2125d80f4583c52c46f8084bcc030c9b'
@@ -19,23 +24,45 @@ SECRET_KEY = 'development key'
 app = Flask(__name__)
 app.config.from_object(__name__)
 db = SQLAlchemy(app)
+db.create_all()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String())
     first_name = db.Column(db.String())
     clef_id = db.Column(db.Integer, unique=True)
+    logged_out_at = db.Column(db.Integer)
+
+
+def logged_in(view):
+    """
+    Decorator that checks whether a user is currently logged in.
+    If a user is logged in it provides the user object.
+    Uses Clef-based database logout.
+    """
+    @functools.wraps(view)
+    def decorated_view(*args, **kwargs):
+        user_id = session.get('user', '')
+        logged_in_at = session.get('logged_in_at', None)
+        user = User.query.get(user_id)
+
+        print session
+        print user
+
+        # does check for database logout of user
+        if user and user.logged_out_at > logged_in_at:
+            session.clear()
+            user = None
+
+        return view(user=user, *args, **kwargs)
+    return decorated_view
+
 
 @app.route('/')
-def hello():
-    user_id = session.get('user')
-    user = User.query.filter_by(id=user_id).first()
+@logged_in
+def hello(user=None):
     return render_template('index.html', user=user)
 
-@app.route('/logout')
-def logout():
-    session.pop('user')
-    return redirect(url_for('hello'))
 
 @app.route('/login')
 def login():
@@ -61,14 +88,55 @@ def login():
     user_info = json_response['info']
     user = User.query.filter_by(clef_id=user_info['id']).first()
     if not user:
-        user = User(email=user_info['email'],
+        user = User(
+            email=user_info['email'],
             first_name=user_info['first_name'],
-            clef_id=user_info['id'])
+            clef_id=user_info['id']
+        )
         db.session.add(user)
         db.session.commit()
 
     session['user'] = user.id
+    session['logged_in_at'] = time.time()
+
     return redirect(url_for('hello'))
+
+
+class LogoutHookException(Exception):
+    pass
+
+
+@app.route('/logout')
+@logged_in
+def logout(user=None):
+    if request.form.get("logout_token", None) is not None:
+        data = dict(
+            logout_token=request.form.get("logout_token"),
+            app_id=CLEF_APP_ID,
+            app_secret=CLEF_APP_SECRET
+        )
+
+        response = requests.post("https://clef.io/api/v1/logout", data=data)
+
+        if response.status_code == 200:
+            json_response = json.loads(response.text)
+
+            if json_response.get('success', False):
+                clef_id = json_response.get('clef_id', None)
+
+                user = User.query.filter_by(clef_id=clef_id).first()
+
+                user.logged_out_at = time.time()
+
+                db.session.add(user)
+                db.session.commit()
+
+                return "ok"
+
+    elif user:
+        session.clear()
+        return redirect(url_for('hello'))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
